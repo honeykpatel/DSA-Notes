1b810eac6cbf98d14e48e913c715bdeb993d259b
Honey Patel <66742927+honeykpatel@users.noreply.github.com>
2021-07-06 12:49:27 +0530

    Create Problem3.py

---
+++ Problem3.py
@@ -0,0 +1,18 @@
+#Method 1 : Using extra array
+
+a1 = [1,4,7,8,10]
+a2 = [2,3,9]
+
+a3 = a1+a2
+#sorting the 3rd array
+a3.sort()
+print(a3)
+
+#putting sorted values back in both arrays
+for i in range(len(a3)):
+    if i<len(a1):
+        a1[i] = a3[i] #first values will go in a1
+    else:
+        a2[i-len(a1)] = a3[i] #rest in a2
+print(a1)
+print(a2)
